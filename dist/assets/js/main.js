4a4e4e48ca48e6c1df4b88fd9d3f056800671d58
sergeykotenko <sergeykotenko@bk.ru>
2023-02-24 09:18:05 +0300

    start project

---
+++ dist/assets/js/main.js
@@ -0,0 +1,15 @@
+console.log(1)
+console.log(1)
+console.log(1)
+console.log(1)
+console.log(1)
+console.log(2)
+console.log(3)
+
+// const menu = document.querySelector('.menu')
+
+// menu.addEventListener('click', () => {
+// 	document.querySelector('.contacts').scrollIntoView({
+// 		behavior: 'smooth',
+// 	})
+// })
---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+//= components/script.js
+//= components/script2.js
+//= components/script3.js
+
+// const menu = document.querySelector('.menu')
+
+// menu.addEventListener('click', () => {
+// 	document.querySelector('.contacts').scrollIntoView({
+// 		behavior: 'smooth',
+// 	})
+// })
