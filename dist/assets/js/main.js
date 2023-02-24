ec53de8bd8244f05e3cfa5f17016f99e379b9644
sergeykotenko <sergeykotenko@bk.ru>
2023-02-24 09:38:25 +0300

    add img

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -9,6 +9,7 @@ console.log(3)
 const menu = document.querySelector('.menu')
 
 menu.addEventListener('click', () => {
+	console.log('11')
 	document.querySelector('.contacts').scrollIntoView({
 		behavior: 'smooth',
 	})
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,6 +5,7 @@
 const menu = document.querySelector('.menu')
 
 menu.addEventListener('click', () => {
+	console.log('11')
 	document.querySelector('.contacts').scrollIntoView({
 		behavior: 'smooth',
 	})
