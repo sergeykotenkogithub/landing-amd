6d805eb98492f5a4c20bfdc6defc613e45de6d47
sergeykotenko <sergeykotenko@bk.ru>
2023-02-25 14:57:26 +0300

    adaptive mobile all blocks

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,10 +1,9 @@
 const menu = document.querySelector('.menu')
 
 menu.addEventListener('click', () => {
-	console.log('11')
 	document.querySelector('.contacts').scrollIntoView({
 		behavior: 'smooth',
 	})
 })
-console.log(2)
-console.log(3)
+// console.log(2)
+// console.log(3)
--- src/assets/js/components/script.js
+++ src/assets/js/components/script.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('.menu')
 
 menu.addEventListener('click', () => {
-	console.log('11')
 	document.querySelector('.contacts').scrollIntoView({
 		behavior: 'smooth',
 	})
